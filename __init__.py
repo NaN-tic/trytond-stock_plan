252bac01c6b744441564ecfa1adeca4b69fdb2fa
Ismael Marcos Martínez <ismael.marcos@nan-tic.com>
2025-06-18 14:57:48 +0200

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# This file is part stock_plan module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.pool import Pool
+
+def register():
+    Pool.register(
+        module='stock_plan', type_='model')
+    Pool.register(
+        module='stock_plan', type_='wizard')
+    Pool.register(
+        module='stock_plan', type_='report')
---
+++ tests/test_module.py
@@ -0,0 +1,11 @@
+# This file is part stock_plan module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class StockPlanTestCase(ModuleTestCase):
+    'Test Stock Plan module'
+    module = 'stock_plan'
+
+del ModuleTestCase
