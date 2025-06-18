0bed66f834bf6e750c406e6f73304c4ceb930ce1
Ismael Marcos Martínez <ismael.marcos@nan-tic.com>
2025-06-18 16:25:48 +0200

    Clear 'tests/__init__.py' content's file.

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file is part stock_plan module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-from .test_stock_plan import suite
-
-__all__ = ['suite']
