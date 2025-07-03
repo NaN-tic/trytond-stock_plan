fe236e4c3d0cbe4c1ab442c4f4f732067c370d0e
Ismael Marcos <ismael.marcos@nan-tic.com>
2025-07-03 16:30:16 +0200

    Add traceability fields on productions (#175017) (#2)
    #175017

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,10 +1,10 @@
 # This file is part stock_plan module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
+from trytond.modules.company.tests import CompanyTestMixin
 from trytond.tests.test_tryton import ModuleTestCase
 
-
-class StockPlanTestCase(ModuleTestCase):
+class StockPlanTestCase(ModuleTestCase, CompanyTestMixin):
     'Test Stock Plan module'
     module = 'stock_plan'
 
